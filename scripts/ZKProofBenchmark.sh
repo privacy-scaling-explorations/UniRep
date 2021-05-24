383b4039e32a65e04ae8eaab2488fc8cd3755450
vivianjeng <vivi432@yahoo.com.tw>
2021-05-24 09:36:18 +0800

    update README.MD

--- scripts/ZKProofBenchmark.sh
+++ scripts/ZKProofBenchmark.sh
@@ -3,30 +3,30 @@
 cd "$(dirname "$0")"
 cd ..
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 4 -ust 4 -ept 4 -nt 128 -epk 4 -a 6 -k 10
+npx ts-node scripts/ZKProofBenchmark.ts -gst 4 -ust 4 -ept 4 -nt 128 -epk 4 -a 4 -k 10
 
 echo '=======================================' &&
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 8 -ust 8 -ept 8 -nt 128 -epk 8 -a 6 -k 10
+npx ts-node scripts/ZKProofBenchmark.ts -gst 8 -ust 8 -ept 10 -nt 64 -epk 4 -a 4 -k 10
 
 echo '=======================================' &&
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 16 -ust 16 -ept 16 -nt 128 -epk 16 -a 6 -k 10
+npx ts-node scripts/ZKProofBenchmark.ts -gst 9 -ust 8 -ept 10 -nt 64 -epk 4 -a 4 -k 10
 
 echo '=======================================' &&
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 64 -ust 64 -ept 64 -nt 128 -epk 64 -a 6 -k 2
+npx ts-node scripts/ZKProofBenchmark.ts -gst 8 -ust 9 -ept 10 -nt 64 -epk 4 -a 4 -k 10
 
 echo '=======================================' &&
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 64 -ust 64 -ept 64 -nt 128 -epk 64 -a 6 -k 2
+npx ts-node scripts/ZKProofBenchmark.ts -gst 8 -ust 8 -ept 11 -nt 64 -epk 4 -a 4 -k 10
 
 echo '=======================================' &&
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 64 -ust 64 -ept 64 -nt 128 -epk 64 -a 6 -k 2
+npx ts-node scripts/ZKProofBenchmark.ts -gst 8 -ust 8 -ept 10 -nt 64 -epk 5 -a 4 -k 10
 
 echo '=======================================' &&
 
-npx ts-node scripts/ZKProofBenchmark.ts -gst 64 -ust 64 -ept 64 -nt 128 -epk 64 -a 6 -k 2
+npx ts-node scripts/ZKProofBenchmark.ts -gst 8 -ust 8 -ept 10 -nt 64 -epk 4 -a 5 -k 10
 
 echo '=======================================' 
